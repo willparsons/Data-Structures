2936956047db530f49b0913c29b3fa540ddabc3c
willparsons <10012418+willparsons@users.noreply.github.com>
2020-06-24 17:47:35 +0100

    Minor changes
    Renamed some method names to resemble their operations clearly.
    
    Included search and is_empty methods.

--- LinkedList/linkedlist.py
+++ LinkedList/linkedlist.py
@@ -5,7 +5,7 @@ class LinkedList:
     def __init__(self, data):
         self.head = Node(data)
 
-    def add(self, data):
+    def insert_at_end(self, data):
         if self.head is None:
             self.head = Node(data)
             return
@@ -17,36 +17,60 @@ class LinkedList:
 
         current.next = Node(data)
 
+    def insert_at_head(self, data):
+        node = Node(data)
+
+        if self.head is not None:
+            node.next = self.head
+
+        self.head = node
+
     def insert_after(self, after, data):
         current = self.head
 
-        while current.next is not None:
+        while current is not None:
             if current.data == after:
                 new_node = Node(data)
-                new_node.next = current.next
+
+                if current.next is not None:
+                    new_node.next = current.next
+
                 current.next = new_node
                 return
             current = current.next
 
         raise Exception("Node not found")
 
-    def remove(self, data):
+    def delete(self, data):
         if self.head.data == data:
             self.head = self.head.next
             return
 
         current = self.head
-
         while current.next is not None:
             if current.next.data == data:
                 current.next = current.next.next
 
             current = current.next
 
-    def replace_head(self, data):
-        node = Node(data)
-        node.next = self.head
-        self.head = node
+    def delete_at_head(self):
+        if self.head.next is not None:
+            self.head = self.head.next
+        else:
+            self.head = None
+
+    def search(self, data):
+        if self.is_empty():
+            return None
+
+        current = self.head
+        while current is not None:
+            if current.data == data:
+                return current
+            current = current.next
+
+    def is_empty(self):
+        return self.head is None
 
     def print_list(self):
         current = self.head
--- LinkedList/main.py
+++ LinkedList/main.py
@@ -3,14 +3,14 @@ from LinkedList.linkedlist import *
 
 def main():
     ll = LinkedList(1)
-    ll.add(2)
-    ll.add(3)
+    ll.insert_at_end(2)
+    ll.insert_at_end(3)
 
-    ll.replace_head(4)
+    ll.insert_at_head(4)
 
     ll.insert_after(2, 6)
 
-    ll.remove(1)
+    ll.delete(1)
 
     ll.print_list()
 
